16df15d25b7bfeb0a03934c90bea722273e5831d
luisforgit <luisforgit@gmail.com>
2025-07-23 17:21:44 +0100

    Delete src/TestTailwind.jsx

--- src/TestTailwind.jsx
+++
@@ -1,8 +0,0 @@
-// src/TestTailwind.jsx
-export default function Test() {
-    return (
-      <div className="bg-[#62BEFF] text-[#0D0D0D] p-4">
-        Teste Tailwind + Node 22
-      </div>
-    )
-  }
